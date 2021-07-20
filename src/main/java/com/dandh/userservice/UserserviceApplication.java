03c8c30a8234cefed8fcea4f32b4a4d3c5f3f685
rabinrai44 <rabinrai44@gmail.com>
2021-07-19 22:06:02 -0400

    Added more api endpoints for create user and role. Adding initial user data

--- src/main/java/com/dandh/userservice/UserserviceApplication.java
+++ src/main/java/com/dandh/userservice/UserserviceApplication.java
@@ -1,13 +1,40 @@
 package com.dandh.userservice;
 
+import com.dandh.userservice.domain.Role;
+import com.dandh.userservice.domain.User;
+import com.dandh.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "John Travolta", "john", "password123", new ArrayList<>()));
+			userService.saveUser(new User(null, "Will Smith", "will", "password123", new ArrayList<>()));
+			userService.saveUser(new User(null, "Martin Matthew", "martin", "password123", new ArrayList<>()));
+			userService.saveUser(new User(null, "Mark Eliezer", "mark", "password123", new ArrayList<>()));
+
+			userService.addRoleToUser("john", "ROLE_USER");
+			userService.addRoleToUser("will", "ROLE_MANAGER");
+			userService.addRoleToUser("martin", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("mark", "ROLE_ADMIN");
+		};
 	}
 
 }
--- src/main/java/com/dandh/userservice/api/UserResource.java
+++ src/main/java/com/dandh/userservice/api/UserResource.java
@@ -1,13 +1,15 @@
 package com.dandh.userservice.api;
 
+import com.dandh.userservice.domain.Role;
 import com.dandh.userservice.domain.User;
 import com.dandh.userservice.service.UserService;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,4 +28,36 @@ public class UserResource {
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+    /**
+     * POST - Add a new user
+     *
+     * @param user - as a request body
+     * @return user
+     */
+    @PostMapping("/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/users/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/user/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+}
+
+
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
